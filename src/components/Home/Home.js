f67200eaa398012d5ab26829bff232de26f3da04
gsantoshb <gsantoshb@gmail.com>
2022-06-13 20:33:03 -0400

    updated pages

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,13 +5,34 @@ import { Link } from "react-router-dom";
 function Home() {
     return (
       <div>
-        <h2>Welcome to Home</h2>
-        
-        <nav>
-        <Link to="/upload">Upload!!!!</Link>
-      </nav>
+        <h2>Tools by GSB Team</h2>
+        <br/>
 
+        <main>
+    <div className="row row-cols-1 row-cols-md-3 mb-3 text-center">
+      <div className="col">
+        <div className="card mb-4 rounded-3 shadow-sm border-success">
+          <div className="card-header py-3 text-white bg-success border-success">
+            <h4 className="my-0 fw-normal">Background Remover</h4>
+          </div>
+          <div className="card-body">
+            <h1 className="card-title pricing-card-title">Free!<small className="text-muted fw-light"></small></h1>
+            <ul className="list-unstyled mt-3 mb-4">
+              <li>Remove background easily!</li>
+              <li>HD Images included!</li>
+              <li>Fast and Secure!</li>
+            </ul>
+            <Link to="/upload">
+            <button type="button" className="w-100 btn btn-lg btn-success">Go to Background remover!</button>
+            </Link>
+          </div>
+        </div>
       </div>
+    </div>
+
+
+  </main>
+</div>
     );
   }
 
--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -5,9 +5,22 @@ import { Link } from "react-router-dom";
 function Upload() {
     return (
       <div>
-        <h2>Welcome to Upload screen</h2>
+
+        <main className="container py-5">
+  <div className="px-4 py-5 my-5 text-center">
+    <img className="d-block mx-auto mb-4" src="background-remover.png" alt="" width="150" height="80" />
+    <h1 className="display-5 fw-bold">Background Remover</h1>
+    <div className="col-lg-6 mx-auto">
+      <p className="lead mb-4">Quickly and Easily Remove backgrounds from Images!!</p>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button type="button" className="btn btn-success btn-lg px-4 gap-3">Upload Image</button>
+      </div>
+    </div>
+  </div>
+
+      </main>
         <nav>
-        <Link to="/">Back to home</Link>
+        <Link to="/">&lt;--- Back to home</Link>
       </nav>
       </div>
     );
