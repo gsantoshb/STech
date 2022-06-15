b93904d04a5dc44023b22de3d767e8139ea30848
gsantoshb <gsantoshb@gmail.com>
2022-06-15 16:59:22 -0400

    almost there

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -1,20 +1,35 @@
-import * as React from "react";
+import React, { useState, useRef } from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios';
+import { Buffer } from "buffer";
+import { saveAs } from 'file-saver';
 
 
 function Upload() {
+
+    const [showLoader, setShowLoader]=useState(false);
+
+    function setLoading(value){
+        setShowLoader(value);
+    }
+
     return (
       <div>
-
-        <main className="container py-5">
-  <div className="px-4 py-5 my-5 text-center">
+      <main className="container py-5">
+  <div className="px-3 py-3 my-2 text-center">
     <img className="d-block mx-auto mb-4" src="background-remover.png" alt="" width="150" height="80" />
     <h1 className="display-5 fw-bold">Background Remover</h1>
     <div className="col-lg-6 mx-auto">
       <p className="lead mb-4">Quickly and Easily Remove backgrounds from Images!!</p>
       <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <button type="button" className="btn btn-success btn-lg px-4 gap-3">Upload Image</button>
+        <UploadButton setLoading={setLoading} />      
       </div>
+      
+      {
+      showLoader?
+      (<div className="spinner-border" role="status" />)
+      :(<div></div>)
+      }
     </div>
   </div>
 
@@ -25,5 +40,47 @@ function Upload() {
       </div>
     );
   }
+
+  function UploadButton({setLoading}) {
+    var FileSaver = require('file-saver');
+
+    const [file, setFile] = useState()
+    const [base64, setBase64]=useState(null);
+    function handleChange(event) {
+      setFile(event.target.files[0])
+    }
+    
+     async function handleSubmit(event) {
+      event.preventDefault();
+      setLoading(true);
+      const url = 'http://localhost:5001/';
+      const formData = new FormData();
+      formData.append('file', file);
+      const config = {
+        headers: {
+          'content-type': 'multipart/form-data',
+          responseType: 'arraybuffer'
+        },
+      };
+      const response=await axios.post(url, formData, config);
+
+      if(response.status==200){
+        setLoading(false);
+       const file = new Blob([response.data], {type:'image/png'}) ;      
+       FileSaver.saveAs(file);
+      }
+
+    }
+  
+    return (
+      <div className="App">
+          <form onSubmit={handleSubmit}>
+            <input type="file" onChange={handleChange}/>
+            <button className="btn btn-lg btn-success" type="submit">Remove background!</button>
+          </form>
+      </div>
+    );
+  }
+  
 
   export default Upload;
